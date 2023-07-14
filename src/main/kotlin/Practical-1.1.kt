f96e0e9fa6b246ee7f198d0228aac4d7d3c03bc2
student <student@gnu.ac.in>
2023-07-14 08:27:52 +0530

    Create a practical 1.1

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,21 @@
+fun main(){
+    var a:Int=5
+    var b:Float=1.5F
+    var c:Char='a'
+    var d:String="Abhay"
+    var e:Boolean=true
+    var f:Double=89.22
+    var g:Long=43435446846
+    var h:Short=-1
+    var i:Byte=100
+    println("Abhay Hingrajiya 22012012024")
+    println("$a is a Integer value")
+    println("$b is a Float value")
+    println("$c is a Character value")
+    println("$d is a String value")
+    println("$e is a Boolean value")
+    println("$f is a Double value")
+    println("$g is a Long value")
+    println("$h is a Short value")
+    println("$i is a Byte value")
+}
