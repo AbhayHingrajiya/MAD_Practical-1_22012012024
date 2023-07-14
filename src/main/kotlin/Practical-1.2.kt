47092b7c5b76a45bfaecf5c03af291c9af5163ba
student <student@gnu.ac.in>
2023-07-14 08:48:13 +0530

    Create a practical 1.2

---
+++ src/main/kotlin/Practical-1.2.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var a:Int=20
+    var b:Double=a.toDouble()
+    var c:String="5"
+    var d:Int=c.toInt()
+    var e:Double=c.toDouble()
+    println("Abhay Hingrajiya 22012012024")
+    println("$b is Double value from integer")
+    println("$d is integer value from string")
+    println("$e is double value from string")
+}
