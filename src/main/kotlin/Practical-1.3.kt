be23f37e502fee319c200d60075371106eb043e5
student <student@gnu.ac.in>
2023-07-18 08:27:00 +0530

    Practical-1.3

---
+++ src/main/kotlin/Practical-1.3.kt
@@ -0,0 +1,28 @@
+fun main(){
+    print("Enter Your Enrollment Number: ");
+    var enNo= readLine();
+    print("Enter Your Name: ");
+    var name= readLine();
+    print("Enter Your Batch: ");
+    var batch= readLine();
+    print("Enter Your Class: ");
+    var yourClass= readLine();
+    print("Enter Your Branch: ");
+    var branch= readLine();
+    print("Enter Your Collage Name: ");
+    var collage= readLine();
+    print("Enter Your University Name: ");
+    var university= readLine();
+    print("Enter Your Age: ");
+    var age= readLine();
+    println("");
+    println("************************");
+    println("Enrollment Number: $enNo");
+    println("Name: $name");
+    println("Batch: $batch");
+    println("Class: $yourClass");
+    println("Branch: $branch");
+    println("Collage Name: $collage");
+    println("University Name: $university");
+    println("Age: $age");
+}
